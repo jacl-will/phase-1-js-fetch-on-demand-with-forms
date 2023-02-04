351a1a6254fffa483c93ff90982d81942afedae5
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-04 13:42:39 -0500

    commit tree c4bdb90e11d5c07e00c54ae24cd5549c66e0490f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => 
+  {event.preventDefault();
+    // used to check that event listener was working 
+    // console.log(event)
+    const input = document.querySelector('input#searchByID');
+    // check to make sure that the value of what is input returns 
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    });
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
